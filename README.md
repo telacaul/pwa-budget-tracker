629f17c9bcaf31658b1feb468a44fe91704d5534
Tela Caul <telacaul@gmail.com>
2021-06-19 17:23:46 -0700

    change mongoose connect

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Budget Tracker Starter Code
+# Budget Tracker
+This application gives users a fast and easy way to track their money with online AND offline functionality. Add expenses and deposits to your budget with or without a connection. 
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
@@ -16,7 +16,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+//MongoDB_URI on line 7
+mongoose.connect("mongodb+srv://tcaul:reMbaT22!@budget.l6ijt.mongodb.net/budget_tracker?retryWrites=true&w=majority", {
+  useUnifiedTopology: true,
   useNewUrlParser: true,
   useFindAndModify: false
 });
